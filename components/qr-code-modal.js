3cfac9efca1573e1b407312b7d4b81fc9d86b989
Kazerlelutin <b@bouteiller.contact>
2023-10-09 01:55:55 +0200

    fix modal

--- components/qr-code-modal.js
+++ components/qr-code-modal.js
@@ -4,13 +4,9 @@ import Qr from 'qrcode'
 import { getShopListById } from '../utils/idb'
 import { convertTxtToBase64 } from '../utils/convert-to-base64'
 
-import { Modal } from './modal'
-
 export const QrCodeModal = {
-  async oninit() {
-    const canvas = document.getElementById('qr-code')
-    if (!canvas) return (state.open = false)
-    const list = await getShopListById(attrs.id)
+  async oncreate(vnode) {
+    const list = await getShopListById(vnode.attrs.id)
 
     const str = list.items
       .map((item) => {
@@ -23,28 +19,15 @@ export const QrCodeModal = {
     const base = convertTxtToBase64(str)
     const url = `${window.location.origin}/#!/import/${base}`
 
+    const canvas = document.getElementById('qr-code')
+
+    if (!canvas) return
+
     Qr.toCanvas(canvas, url, (error) => {
       if (error) console.error(error)
     })
   },
-  view(vnode) {
-    const { state } = vnode
-    return m('div', '', [
-      m(
-        'button',
-        {
-          class: 'flex justify-center gap-2 uppercase font-bold',
-          type: 'button',
-          onclick: async () => {},
-        },
-        'Partager'
-      ),
-      state.open &&
-        m(Modal, {
-          open: state.open,
-          onclose: () => (state.open = false), // <-- Use state here
-          children: m('canvas', { id: 'qr-code' }),
-        }),
-    ])
+  view() {
+    return m('span')
   },
 }
